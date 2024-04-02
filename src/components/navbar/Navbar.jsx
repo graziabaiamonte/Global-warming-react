369c220c84a7968d8c1b99725b6944239c2f9600
graziabaiamonte <graziabaiamonte19@gmail.com>
2024-04-02 22:43:07 +0200

    proa

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import styles from './navbar.module.css';
 
+import {  Link  } from 'react-router-dom';
+
 
 function Navbar() {
     return (
         <div className={styles.navbar}>
-
             <div className={styles.boxH1}>
-                <a className={styles.textH1} href="/"><h1>GLOBAL WARMING</h1></a>
+                <Link to="/home">GLOBAL </Link><a className={styles.textH1} href="/"><h1>GLOBAL WARMING</h1></a>
             </div>
-
-
-
         </div>
     )
 }
