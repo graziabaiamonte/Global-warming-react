05354338834db315c821540d1667bf2e56342c5d
graziabaiamonte <graziabaiamonte19@gmail.com>
2024-05-30 10:21:24 +0200

    modify repository name

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ import { BrowserRouter, Routes, Route,  } from 'react-router-dom';
 function App() {
   return (
     <div className={styles.appPage}>
-      <BrowserRouter  basename="/React-Project">
+      <BrowserRouter  basename="/Global-warming-react">
         <Navbar />
         <Routes>
           <Route index element={<Home />} />
